ea90aca195074e60e6cb5ee8c99f4fb04c2b4d2c
chamodSamajith <chamodsr@gmail.com>
2023-10-26 22:17:14 +1100

    tutor details update and delete APIs

--- Routes/TutorDetailsRoutes.js
+++ Routes/TutorDetailsRoutes.js
@@ -44,5 +44,60 @@ router.get('/getAll', (req, res) => {
         });
 });
 
+//update API
+// Update a tutor by ID
+router.put('/update/:id', (req, res) => {
+    const tutorId = req.params.id;
+    const updatedData = {
+        TutorName: req.body.TutorName,
+        TutorSubjects: req.body.TutorSubjects,
+        AvailableDateTIme: req.body.AvailableDateTIme
+    };
+
+    Tutor.findByIdAndUpdate(tutorId, updatedData, { new: true })
+        .then(updatedTutor => {
+            if (!updatedTutor) {
+                return res.status(404).send({
+                    message: 'Tutor not found'
+                });
+            }
+            res.status(200).send({
+                message: 'TutorDetails updated successfully!',
+                data: updatedTutor
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while updating tutor details',
+                error: err
+            });
+        });
+});
+
+//deete tutor
+// Delete a tutor by ID
+router.delete('/delete/:id', (req, res) => {
+    const tutorId = req.params.id;
+
+    Tutor.findByIdAndRemove(tutorId)
+        .then(deletedTutor => {
+            if (!deletedTutor) {
+                return res.status(404).send({
+                    message: 'Tutor not found'
+                });
+            }
+            res.status(200).send({
+                message: 'TutorDetails deleted successfully!',
+                data: deletedTutor
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while deleting tutor details',
+                error: err
+            });
+        });
+});
+
 
 module.exports = router;
