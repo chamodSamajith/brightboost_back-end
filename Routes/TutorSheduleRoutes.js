065bbfd680c792a9f5328c0c5160139c4efb37b5
chamodSamajith <chamodsr@gmail.com>
2023-10-26 22:50:28 +1100

    getall,getbyid,update,delte APIs

--- Routes/TutorSheduleRoutes.js
+++ Routes/TutorSheduleRoutes.js
@@ -29,3 +29,92 @@ router.post('/create', (req, res) => {
 
     })
 })
+
+//get all tutor schedules
+router.get('/getAll', (req, res) => {
+    Tutor.find()
+        .then(schedules => {
+            res.status(200).send({
+                message: 'Tutor Schedules retrieved successfully!',
+                data: schedules
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while retrieving tutor schedules',
+                error: err
+            });
+        });
+});
+
+// get a tutor schedule by ID
+router.get('/getById/:id', (req, res) => {
+    const scheduleId = req.params.id;
+
+    Tutor.findById(scheduleId)
+        .then(schedule => {
+            if (!schedule) {
+                return res.status(404).send({
+                    message: 'Tutor Schedule not found'
+                });
+            }
+            res.status(200).send({
+                message: 'Tutor Schedule retrieved successfully!',
+                data: schedule
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while retrieving tutor schedule',
+                error: err
+            });
+        });
+});
+
+// Update a tutor schedule by ID
+router.put('/update/:id', (req, res) => {
+    const scheduleId = req.params.id;
+
+    Tutor.findByIdAndUpdate(scheduleId, req.body, { new: true })
+        .then(updatedSchedule => {
+            if (!updatedSchedule) {
+                return res.status(404).send({
+                    message: 'Tutor Schedule not found'
+                });
+            }
+            res.status(200).send({
+                message: 'Tutor Schedule updated successfully!',
+                data: updatedSchedule
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while updating tutor schedule',
+                error: err
+            });
+        });
+});
+
+// Delete a tutor schedule by ID
+router.delete('/delete/:id', (req, res) => {
+    const scheduleId = req.params.id;
+
+    Tutor.findByIdAndRemove(scheduleId)
+        .then(deletedSchedule => {
+            if (!deletedSchedule) {
+                return res.status(404).send({
+                    message: 'Tutor Schedule not found'
+                });
+            }
+            res.status(200).send({
+                message: 'Tutor Schedule deleted successfully!',
+                data: deletedSchedule
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: 'Error while deleting tutor schedule',
+                error: err
+            });
+        });
+});
